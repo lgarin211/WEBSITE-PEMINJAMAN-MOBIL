d201d3466c9645fbc1e0070e7652db986a051c36
Agustinus <lagustinus211@gmail.com>
2023-06-12 14:47:33 +0700

    pass

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
 
 <body>
 
-    <nav class="container navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="container navbar navbar-expand-lg navbar-light bg-light fixed-top">
         <a class="navbar-brand " href="#">Daftar Mobil</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
